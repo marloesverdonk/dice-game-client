2bd059f3d9a4c25e614f686858b272dae74dcc70
P Panthong <panthari.panthong@gmail.com>
2019-10-29 11:11:58 +0100

    game logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+
 import Rooms from './components/Rooms'
 import Signup from './components/Signup';
 import Login from './components/Login'
+import GameContainer from './components/GameContainer'
+
 import { Route, Link } from "react-router-dom";
 
 function App() {
@@ -11,14 +14,16 @@ function App() {
       <header className="App-header">
       </header>
       <main>
-      <div>
-        <Link to="/"> Sign up </Link>
-        <Link to="/login"> Login</Link>
-      
-        <Route path="/" exact component={Signup} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/rooms" exact component={Rooms} />
-      </div>
+        <div>
+          <Link to="/"> Sign up </Link>
+          <Link to="/login"> Login</Link>
+          <Link to='/game'>Game</Link>
+
+          <Route path="/" exact component={Signup} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/rooms" exact component={Rooms} />
+          <Route path="/game" exact component={GameContainer} />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/Game.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    const value = this.props.value
+    return (
+      <div>
+        <h1>Play game</h1>
+        <h1>Dice1</h1>
+        <h1>{value.dice1}</h1>
+        <h1>Dice2</h1>
+        <h1>{value.dice2}</h1>
+        <h1>Round Score</h1>
+        <h1>{value.roundScore}</h1>
+        <button onClick={this.props.rollDice}>ROLL DICE</button>
+        <button>HOLD</button>
+      </div>
+    );
+  }
+}
+
+export default Game;
